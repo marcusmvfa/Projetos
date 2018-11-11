343f083d22e6f463fa541273c6c92607616128b6
Marcus <Marcus@DESKTOP-JRM6HH2>
2018-11-11 14:04:01 -0200

    Exercicio 10_4 pronto

--- Exercicios-10/src/exercicio_10_4/TesteArqPonto2D.java
+++ Exercicios-10/src/exercicio_10_4/TesteArqPonto2D.java
@@ -1,12 +1,16 @@
 package exercicio_10_4;
 
+import edu.udc.psw.CriarArquivoSerial;
 import edu.udc.psw.LerArqTexto;
+import edu.udc.psw.LerArquivoSerial;
 
 public class TesteArqPonto2D {
 
 	public static void main(String[] args) {
 		CriarArq f = new CriarArq();
 		LeituraArquivo l = new LeituraArquivo();
+		//CriarArquivoSerial criararquivoSerial = new CriarArquivoSerial();
+		//LerArquivoSerial lerarquivoserial = new LerArquivoSerial();
 		
 		f.openFile();
 		f.adcionaRegistros();
@@ -15,7 +19,15 @@ public class TesteArqPonto2D {
 		l.openFile();
 		l.lerArquivo();
 		l.closeFile();
-
+/*
+		criararquivoSerial.open();
+		criararquivoSerial.adicionaRegistro();
+		criararquivoSerial.closeFile();
+		
+		lerarquivoserial.openFile();
+		lerarquivoserial.lerArquivo();
+		lerarquivoserial.closeFile();
+*/
 	}
 
 }
